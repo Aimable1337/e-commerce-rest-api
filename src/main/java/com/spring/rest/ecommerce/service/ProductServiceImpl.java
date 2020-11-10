c2d514f753bf2ddda3f9f38d046a7d91d1bd8f9e
Milosz Nieradka <milosznieradka@gmail.com>
2020-11-10 13:03:19 +0100

    ADD ProductService

---
+++ src/main/java/com/spring/rest/ecommerce/service/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.spring.rest.ecommerce.service;
+
+import com.spring.rest.ecommerce.entity.Product;
+import com.spring.rest.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository theProductRepository){
+        productRepository = theProductRepository;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product findByID(long theId) {
+        if (productRepository.findById(theId).isPresent()){
+            return productRepository.findById(theId).get();
+        } else {
+            throw new RuntimeException("Did not found product by id - " + theId);
+        }
+    }
+
+    @Override
+    public void save(Product theProduct) {
+        productRepository.save(theProduct);
+    }
+
+    @Override
+    public void deleteByID(long theId) {
+        productRepository.deleteById(theId);
+    }
+}
