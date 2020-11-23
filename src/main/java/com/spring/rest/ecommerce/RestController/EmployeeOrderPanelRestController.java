3a34f634215c91acdfe0fc02f1124cf388ed8edd
Milosz Nieradka <milosznieradka@gmail.com>
2020-11-23 23:04:12 +0100

    REFACTORING implement methods for EmployeeOrderPanelRestController

--- src/main/java/com/spring/rest/ecommerce/RestController/EmployeeOrderPanelRestController.java
+++ src/main/java/com/spring/rest/ecommerce/RestController/EmployeeOrderPanelRestController.java
@@ -4,18 +4,20 @@ import com.spring.rest.ecommerce.entity.Order;
 import com.spring.rest.ecommerce.headers.HeaderGenerator;
 import com.spring.rest.ecommerce.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employee/order-panel")
 public class EmployeeOrderPanelRestController { //TODO: implement methods
 
-    private OrderService orderService;
+    private final OrderService orderService;
 
-    private HeaderGenerator headerGenerator;
+    private final HeaderGenerator headerGenerator;
 
     @Autowired
     public EmployeeOrderPanelRestController(OrderService orderService,
@@ -25,23 +27,42 @@ public class EmployeeOrderPanelRestController { //TODO: implement methods
     }
 
     @GetMapping("/orders")
-    public ResponseEntity<List<Order>> getAllOrders() throws Exception{
-        throw new Exception("Not implemented yet!");
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return new ResponseEntity<>(
+                orderService.findAll(),
+                headerGenerator.getHeadersForSuccessGetMethod(),
+                HttpStatus.OK
+        );
     }
 
     @GetMapping("/orders/{theId}")
-    public ResponseEntity<Order> getOrderBuId(@PathVariable long theId) throws Exception{
-        throw new Exception("Not implemented yet!");
+    public ResponseEntity<Order> getOrderBuId(@PathVariable long theId) {
+        return new ResponseEntity<>(
+                orderService.findOrderById(theId),
+                headerGenerator.getHeadersForSuccessGetMethod(),
+                HttpStatus.OK
+        );
     }
 
     @PostMapping("/orders")
-    public ResponseEntity<Order> createOrder(@RequestBody Order theOrder) throws Exception{
-        throw new Exception("Not implemented yet!");
+    public ResponseEntity<Order> createOrder(@RequestBody Order theOrder, HttpServletRequest request) {
+        theOrder.setOrderId(0);
+        orderService.save(theOrder);
+        return new ResponseEntity<>(
+                theOrder,
+                headerGenerator.getHeadersForSuccessPostMethod(request, theOrder.getOrderId()),
+                HttpStatus.CREATED
+        );
     }
 
     @PutMapping("/orders")
-    public ResponseEntity<?> updateOrder(@RequestBody Order theOrder) throws Exception{
-        throw new Exception("Not implemented yet!");
+    public ResponseEntity<String> updateOrder(@RequestBody Order theOrder) {
+        orderService.save(theOrder);
+        return new ResponseEntity<>(
+                "Order updated successfully!",
+                headerGenerator.getHeadersForSuccessGetMethod(),
+                HttpStatus.ACCEPTED
+        );
     }
 
     @PutMapping("/orders/{theId}")
@@ -50,7 +71,12 @@ public class EmployeeOrderPanelRestController { //TODO: implement methods
     }
 
     @DeleteMapping("/orders/{theId}")
-    public ResponseEntity<?> deleteOrder(@PathVariable long theId) throws Exception {
-        throw new Exception("Not implemented yet!");
+    public ResponseEntity<String> deleteOrder(@PathVariable long theId) throws Exception {
+        orderService.deleteById(theId);
+        return new ResponseEntity<>(
+                "Order deleted!",
+                headerGenerator.getHeadersForSuccessGetMethod(),
+                HttpStatus.ACCEPTED
+        );
     }
 }
